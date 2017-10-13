56a59453b03c19ff7591594cdfdf2fce91cea5e0
John Perdue <john_perdue05@yahoo.com>
2017-10-12 20:17:53 -0500

    Completed build-a-blog and bonus missions, including timestamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,10 +15,14 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(1000))
+    pub_date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, pub_date=None):
         self.title = title
         self.body = body
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
 
 @app.route('/')
 def index():
@@ -27,7 +32,7 @@ def index():
 def blog():
     id = request.args.get('id')
     if not id:
-        posts = Blog.query.all()
+        posts = Blog.query.order_by(Blog.pub_date.desc()).all()
         return render_template('blog.html',title="Build a Blog", 
         posts=posts)
     post = Blog.query.filter_by(id=id).first()
